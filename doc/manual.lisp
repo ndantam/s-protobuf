95dbddb6f81375d12969f915095b8ba00ef99824
Neil T. Dantam <ntd@gatech.edu>
2009-09-27 02:14:52 -0400

    Added usage documentation

--- doc/manual.lisp
+++ doc/manual.lisp
@@ -61,8 +61,8 @@
                                protoc to generate lisp code
                                (leave C++ to the masochists), but
                                rather read in the protobuf-encoded
-                               FileDescriptorSet output of protoc and
-                               compile that."))
+                               FileDescriptorSet output of" (command
+                               protoc) "and compile that."))
                    (listitem (para "The varint handling code supports
                               arbitrary precision."))
                    (listitem (para " We don't track unknown fields"))))
@@ -70,24 +70,67 @@
                   (para "Varints are strictly unsigned (svarints are
                     signed).  Cutting things off at 10 bytes and
                     making it negative is a kludge."))
+           (:sect 2 "Bugs"
+                  (itemizedlist
+                   (listitem (para "Packages -- sort of supported, but probably not too well"))))
            (:sect 2 "Missing Features"
                   (itemizedlist
                    (listitem (para "Skipping Unknown Fields"))
                    (listitem (para "Default Values"))
-                   (listitem (para "Packages"))
-                   (listitem (para "Trackng Unknown Fields"))
+                   (listitem (para "Tracking Unknown Fields"))
                    (listitem (para "Support for (#-sbcl) -- only
                                     problem area is floating-point
                                     coding"))
                    (listitem (para "Extensions"))
                    (listitem (para "Services"))
+                   ))
+           (:sect 2 "Possible Improvements"
+                  (itemizedlist
+                   (listitem (para "Some refactoring of protoc.lisp would help"))
+                   (listitem (para "ASDF Integration"))
                    (listitem (para "Packed length-delimited
                                     fields (Google may not support it,
                                     but we could)")))))
     (:sect 1 "Usage"
-           (para "Coming Soon"))))
-
-
+           (:sect 2 "Using .proto files"
+                  (para "S-PROTOBUF reads in the binary output of"
+                  (command protoc)".  Thus, you should first run your
+                  .proto file through" (command protoc) "using the -o
+                  option: ")
+                  (programlisting "$ protoc -omyfile.protobin myfile.proto")
+                  (para "Then you can use the macro
+                  function " (function "protoc:load-proto-set") "
+                  which reads in the .protobin and expands into the
+                  definitions for classes, functions, and methods.")
+                  (programlisting "(require :s-protobuf)"
+                                  "(protoc:load-proto-set \"myfile.protobin\")")
+                  )
+           (:sect 2 "Writing protobuf's as S-Expressions"
+                  (para "If curly braces make you angry, you can also
+                  write the protocol buffer definitions as
+                  S-Expressions.  Here is documentation by example:")
+                  (programlisting 
+                   "(require :s-protobuf)"
+                   ,(string #\Newline)
+                   ";; A simple message"
+                   "(protoc:def-proto-msg test1"
+                   "  (field a :int32 1))"
+                   ,(string #\Newline)
+                   ";; A nested message"
+                   "(protoc:def-proto-msg test3"
+                   "    (field c test1 3))"
+                   ,(string #\Newline)
+                   ";; repeated fields"
+                   "(protoc:def-proto-msg test4
+                       (field d :int32 4 :repeated t :packed nil))"
+                   ,(string #\Newline)
+                   ";; enums"
+                   "(protoc:def-proto-msg testx2"
+                   "  (enum e (:a 0) (:b 10) (:c 20))"
+                   "  (field a testx2-e 1 :repeated nil :packed nil))"
+                  )
+                  (para "If you want some more details, you can try macroexpanding " (function load-proto-set) ".")))))
+           
 
 (defun expand-inline ()
   (s-expand t *doc* :transform-alist *docbook-transform-alist*))
